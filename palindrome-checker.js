a3294c88ba15ffb36a53b383dbbda6875aea44b3
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2019-01-25 12:28:52 +0000

    Create palindrome-checker.js
    reuploading file

---
+++ palindrome-checker.js
@@ -0,0 +1,14 @@
+function palindrome(str) {
+
+    var reverseStr = ''
+  
+    //removes all non-alphanumeric characters
+    str = str.replace(/\W|_|,/g, "").toLowerCase()
+    
+    //reverses the string
+    reverseStr = str.split("").reverse().join("")
+    
+    //checks for palindrome
+    return str === reverseStr;
+  }
+  
