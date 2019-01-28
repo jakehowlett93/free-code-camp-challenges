5b0848df7daba48e9e84d6451ba7987a8c97f56c
Jake Howlett <41288146+jakehowlett93@users.noreply.github.com>
2019-01-28 10:48:29 +0000

    Create caesars-cipher.js
    The third challenge required for the free code camp javascript certification

---
+++ javascript/caesars-cipher.js
@@ -0,0 +1,23 @@
+function rot13(str) {
+  
+    var arrayStr = str.split(""), asciiValue, codedLetter;
+  
+  //convert letters to ascii
+   arrayStr.forEach(function (value, index, arr){
+      asciiValue = str.charCodeAt(index);
+      //encodes the letters
+      if (asciiValue >= 65 && asciiValue <= 90) {
+        asciiValue += 13;
+        //loops round the alphabet
+        if (asciiValue > 90) {
+          asciiValue -= 26;
+        }
+      }
+      //replaces letters in the array
+      codedLetter = String.fromCharCode(asciiValue);
+      arrayStr.splice(index, 1, codedLetter);
+  
+    });
+  
+    return arrayStr.join("");
+  }
